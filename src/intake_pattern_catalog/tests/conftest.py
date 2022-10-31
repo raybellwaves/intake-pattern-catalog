96ef8323cd0abf89ad50452979d7356e0c5a2cab
Ray Bell <ray.bell@dtn.com>
2022-10-31 16:48:43 -0400

    commit

--- src/intake_pattern_catalog/tests/conftest.py
+++ src/intake_pattern_catalog/tests/conftest.py
@@ -44,10 +44,11 @@ class MockHttpClientResponse(aiohttp.client_reqrep.ClientResponse):
     @property
     def raw_headers(self) -> aiohttp.typedefs.RawHeaders:
         # Return the headers encoded the way that aiobotocore expects them
-        return {
-            k.encode("utf-8"): str(v).encode("utf-8")
-            for k, v in self.response.headers.items()
-        }.items()
+        # return {
+        #     k.encode("utf-8"): str(v).encode("utf-8")
+        #     for k, v in self.response.headers.items()
+        # }.items()
+        return None
 
 
 @pytest.fixture
